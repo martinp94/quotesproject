a9579c08b1a881a62dd752c587e51d713f851bc1
Martin Popovic <martinpopovicfit@gmail.com>
2018-09-04 17:15:50 +0200

    Created components

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,24 @@
 </template>
 
 <script>
+	
+	import Header from './components/Header.vue';
+	import AddQuotes from './components/AddQuotes.vue';
+	import QuotesList from './components/QuotesList.vue';
+	import Footer from './components/Footer.vue';
+
     export default {
-        
+        data() {
+        	return {
+        		
+        	}
+        },
+        components: {
+        	appHeader: Header,
+        	appAddQuotes: AddQuotes,
+        	appQuotesList: QuotesList,
+        	appFooter: Footer
+        }
     }
 </script>
 
